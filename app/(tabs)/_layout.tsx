5bd084c71fa6ea55e18049dc7fbb4db041e17b72
Green Arrow <gowthamanganesan021@gmail.com>
2025-01-01 00:28:48 +0000

    some changes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -36,8 +36,8 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: 'Scoreboard',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="table.fill" color={color} />,
         }}
       />
     </Tabs>
